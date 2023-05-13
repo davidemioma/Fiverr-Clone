666d457895fb22b72726828a3ee4699443be5679
david emioma <davidemiomauche@gmail.com>
2023-05-13 23:14:13 +0100

    Built home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,198 @@
-import Image from 'next/image'
+import React from "react";
+import { cards, projects } from "../util/data";
+import Faetured from "./components/featured/Faetured";
+import TrustedBy from "./components/trustedBy/TrustedBy";
+import Slider from "./components/Slider";
+import CategoryCard from "./components/CategoryCard";
+import Container from "./components/Container";
+import ProjectCard from "./components/ProjectCard";
 
-export default function Home() {
+const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className="w-screen">
+      <Faetured />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <TrustedBy />
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <Slider>
+        {cards.map((card) => (
+          <CategoryCard key={card.id} card={card} />
+        ))}
+      </Slider>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <section className="bg-[#f1fdf7] w-screen py-16">
+        <Container>
+          <div className="flex flex-col lg:flex-row items-center gap-20">
+            <div className="flex flex-col space-y-4 text-xs md:text-sm">
+              <h1 className="text-lg md:text-xl font-medium">
+                A whole world of freelance talent at your fingertips
+              </h1>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+              <div className="flex flex-col space-y-2 text-[gray]">
+                <div className="flex items-center space-x-2">
+                  <img
+                    className="w-5 h-5 object-cover"
+                    src="/assets/check.png"
+                    alt=""
+                  />
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+                  <p className="text-sm md:text-base font-medium">
+                    The best for every budget
+                  </p>
+                </div>
+
+                <p>
+                  Find high-quality services at every price point. No hourly
+                  rates, just project-based pricing.
+                </p>
+              </div>
+
+              <div className="flex flex-col space-y-2 text-[gray]">
+                <div className="flex items-center space-x-2">
+                  <img
+                    className="w-5 h-5 object-cover"
+                    src="/assets/check.png"
+                    alt=""
+                  />
+
+                  <p className="text-sm md:text-base font-medium">
+                    Quality work done quickly
+                  </p>
+                </div>
+
+                <p>
+                  Find the right freelancer to begin working on your project
+                  within minutes.
+                </p>
+              </div>
+
+              <div className="flex flex-col space-y-2 text-[gray]">
+                <div className="flex items-center space-x-2">
+                  <img
+                    className="w-5 h-5 object-cover"
+                    src="/assets/check.png"
+                    alt=""
+                  />
+
+                  <p className="text-sm md:text-base font-medium">
+                    Protected payments, every time
+                  </p>
+                </div>
+
+                <p>
+                  Always know what you'll pay upfront. Your payment isn't
+                  released until you approve the work.
+                </p>
+              </div>
+
+              <div className="flex flex-col space-y-2 text-[gray]">
+                <div className="flex items-center space-x-2">
+                  <img
+                    className="w-5 h-5 object-cover"
+                    src="/assets/check.png"
+                    alt=""
+                  />
+
+                  <p className="text-sm md:text-base font-medium">
+                    24/7 support
+                  </p>
+                </div>
+
+                <p>
+                  Find high-quality services at every price point. No hourly
+                  rates, just project-based pricing.
+                </p>
+              </div>
+            </div>
+
+            <div className="w-full flex items-center justify-center">
+              <video
+                className="w-full max-w-[720px]"
+                src="/assets/video.mp4"
+                controls
+              />
+            </div>
+          </div>
+        </Container>
+      </section>
+
+      <section className="bg-[#0d084d] text-white w-screen py-16">
+        <Container>
+          <div className="flex flex-col lg:flex-row items-center gap-20">
+            <div className="flex flex-col space-y-4 text-xs md:text-sm">
+              <h1 className="text-lg md:text-xl font-medium">
+                liverr <i>business</i>
+              </h1>
+
+              <h1 className="text-lg md:text-xl font-medium">
+                A business solution designed for <i>teams</i>
+              </h1>
+
+              <p>
+                Upgrade to a curated experience packed with tools and benefits,
+                dedicated to businesses
+              </p>
+
+              <div className="flex items-center space-x-2">
+                <img
+                  className="w-5 h-5 object-cover"
+                  src="/assets/check.png"
+                  alt=""
+                />
+
+                <p>Connect to freelancers with proven business experience</p>
+              </div>
+
+              <div className="flex items-center space-x-2">
+                <img
+                  className="w-5 h-5 object-cover"
+                  src="/assets/check.png"
+                  alt=""
+                />
+
+                <p>
+                  Get matched with the perfect talent by a customer success
+                  manager
+                </p>
+              </div>
+
+              <div className="flex items-center space-x-2">
+                <img
+                  className="w-5 h-5 object-cover"
+                  src="/assets/check.png"
+                  alt=""
+                />
+
+                <p>
+                  Manage teamwork and boost productivity with one powerful
+                  workspace
+                </p>
+              </div>
+
+              <div className="flex justify-center lg:justify-start">
+                <button className="bg-[#1dbf73] px-4 py-2 rounded font-medium">
+                  Explore Liverr Business
+                </button>
+              </div>
+            </div>
+
+            <div className="w-full flex items-center justify-center">
+              <img
+                className="w-full max-w-[720px]"
+                src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_870,dpr_2.0/v1/attachments/generic_asset/asset/d9c17ceebda44764b591a8074a898e63-1599597624768/business-desktop-870-x2.png"
+              />
+            </div>
+          </div>
+        </Container>
+      </section>
+
+      <Slider>
+        {projects.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default Home;
